96ceea97fef6cc2090213ef5da656dd64e683af8
Katrina Lyman <lymankatrina@gmail.com>
2024-03-02 15:13:50 -0700

    update swagger, lint and format

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const authController = require('../controllers/authController');
-const { requiresAuth } = requires('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 // req.isAuthenticated is provided from the auth router
 routes.get(
@@ -9,8 +9,10 @@ routes.get(
   authController.checkAuthStatus // '#swagger.ignore = true'
 );
 
-routes.get('/profile', requiresAuth(), authController.checkProfile // '#swagger.ignore = true'
+routes.get(
+  '/profile',
+  requiresAuth(),
+  authController.checkProfile // '#swagger.ignore = true'
 );
 
 module.exports = routes;
-
