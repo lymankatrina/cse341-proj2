d2157191cfb3f6e854039b9d7b9fc877f2021b9a
Katrina Lyman <lymankatrina@gmail.com>
2024-03-02 15:12:04 -0700

    deploy auth to render

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,3 +2,7 @@
 exports.checkAuthStatus = (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 };
+
+exports.checkProfile = (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+};
--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,11 +1,11 @@
 exports.welcomeMessage = (req, res) => {
-  const localApiDoc = 'http://localhost:3000/swagger/api-docs';
-  const renderApiDoc = 'https://cse341proj2.onrender.com/swagger/api-docs/';
+  const localLogin = 'http://localhost:3000/login';
+  const renderLogin = 'https://cse341proj2.onrender.com/login';
 
   if (req.hostname === 'localhost' && req.protocol === 'http') {
-    res.json({ message: `Welcome, to see documentation visit ${localApiDoc}` });
+    res.json({ message: `Welcome, to see documentation visit ${localLogin}` });
   } else if (req.hostname === 'cse341proj2.onrender.com' && req.protocol === 'https') {
-    res.json({ message: `Welcome, to see documentation visit ${renderApiDoc}` });
+    res.json({ message: `Welcome, to see documentation visit ${renderLogin}` });
   } else {
     res.status(404).json({ error: 'Not Found' });
   }
